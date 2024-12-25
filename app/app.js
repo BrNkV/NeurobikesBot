21f0963b447a6c24b72d61bb1ce1b6a8f38c176c
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2024-12-25 21:56:51 +0300

    fix: nodemon

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ bot.on('message', (msg) => {
 
   // Simple greeting response
   if (text.includes('hi')) {
-    bot.sendMessage(chatId, `'Hello dear user!'+'Chat ID ${chatId} 1dsd'`);
+    bot.sendMessage(chatId, `'Hello dear user!'+'Chat ID ${chatId} 1dsd 123aa'`);
     console.log(chatId);
   } else {
     bot.sendMessage(chatId, 'I am a simple bot. Send "hi" to get a greeting!');
