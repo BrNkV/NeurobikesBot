3d3024426c20d3c29080bd2623664b286b80dc0b
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2024-12-25 21:51:24 +0300

    cl msg

--- app/app.js
+++ app/app.js
@@ -10,11 +10,12 @@ const bot = new TelegramBot(token, { polling: true });
 // Listen for any incoming messages
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  console.log(msg);
   const text = msg.text.toLowerCase();
 
   // Simple greeting response
   if (text.includes('hi')) {
-    bot.sendMessage(chatId, `'Hello dear user!'+'Chat ID ${chatId}'`);
+    bot.sendMessage(chatId, `'Hello dear user!'+'Chat ID ${chatId} 1dsd'`);
     console.log(chatId);
   } else {
     bot.sendMessage(chatId, 'I am a simple bot. Send "hi" to get a greeting!');
